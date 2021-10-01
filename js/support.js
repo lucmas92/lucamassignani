0c99fda14fffd3b4daaaf74520a03e8f7eb02303
lucmas92 <github6>
2021-10-01 16:24:23 +0200

    aggiunto target="_blank" ai link amazon aggiunto input per link referral personalizzati

--- js/support.js
+++ js/support.js
@@ -7,41 +7,70 @@ app.component('products-list', {
   props: ['filename'],
   data() {
     return {
-      tag:"lmassignani-21",
+      tag: "lmassignani-21",
       products: [],
       loading: 'loading...',
-      customLink:''
+      customLink: '',
+      linkReferral: ''
     }
   },
-  methods:{
-    goTo: function(link) {
+  watch: {
+    customLink: async function (val) {
+      console.log(val);
+      if (!val.includes('http')) {
+        this.linkReferral = "https://www.amazon.it/dp/" + val.trim()
+      } else {
+        this.linkReferral = val.trim()
+      }
+      this.linkCheck = false;
+      this.linkReferral += "?tag=" + this.tag
+      try {
+        const headers = {
+          "User-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+          "Authority": "www.amazon.it",
+          "Access-Control-Allow-Origin": "*"
+        };
+        fetch(this.linkReferral, {headers})
+          .then(async response => {
+            console.log(response.status)
+            this.linkCheck = true
+          })
+          .catch(error => {
+          });
+      } catch (e) {
+
+      }
+
+    }
+  },
+  methods: {
+    goTo: function (link) {
       console.log(link);
       window.open(link);
     },
-    getVueObject: function(item){
-          let tmp = {};
-          for (let d in item){
-            tmp[d] = item[d];
-          }
-          return tmp;
+    getVueObject: function (item) {
+      let tmp = {};
+      for (let d in item) {
+        tmp[d] = item[d];
+      }
+      return tmp;
     },
-    async loadJsonFile(){
-       fetch(this.filename)
-         .then(response => response.json())
-         .then(jsonResponse => {
-            if (jsonResponse) {
-              this.loading = '';
-              this.products_md5 = jsonResponse.md5;
-              let pp = jsonResponse.products;
+    async loadJsonFile() {
+      fetch(this.filename)
+        .then(response => response.json())
+        .then(jsonResponse => {
+          if (jsonResponse) {
+            this.loading = '';
+            let pp = jsonResponse.products;
 
-              pp.forEach((product) => {
-                let tmp = this.getVueObject(product);
-                this.products.push(tmp);
-              });
-            }
-          })   
+            pp.forEach((product) => {
+              let tmp = this.getVueObject(product);
+              this.products.push(tmp);
+            });
+          }
+        })
     },
-    async catchWithTryCatch () {
+    async catchWithTryCatch() {
       try {
         await this.loadJsonFile();
       } catch (error) {
@@ -50,12 +79,23 @@ app.component('products-list', {
       }
     }
   },
-  mounted(){
+  mounted() {
     this.catchWithTryCatch();
   },
   template: `
   <div>
-      <span v-if="customLink.length > 0 && customLink.includes('http')">{{customLink}}?tag={{tag}}</span>
+    <div class="grid grid-cols-1 pt-14 px-3 gap-x-12">
+      <h3 class="mb-0">
+        Se non trovi articoli di tuo interesse, inserisci il link sotto, ti verrà fornito un link referral.
+      </h3>
+      <input class="px-2 form-control w-full" placeholder="Inserisci il link all'articolo che vuoi acquistare" type="text" v-model="customLink">
+      <span v-if="customLink.length > 0 && !linkCheck" class="text-danger">
+        Link non valido!
+      </span>
+      <span v-if="customLink.length > 0 && linkCheck">
+        <a :href="linkReferral">https://www.amazon.it/</a>
+      </span>
+    </div>
     <div class="grid grid-cols-1 2xl:grid-cols-3 xl:grid-cols-2 pt-14 gap-x-12">
       {{loading}}
       <div data-aos="fade-up" data-aos-offset="200" v-on:click="goTo(product.link)" class="cursor-pointer m-3 p-3 border-2 hover:border-gray-300" v-for="product in products">
