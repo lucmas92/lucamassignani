827e47ac726c01129a6d3024187314745725c915
Luca <massignani.luca@hotmail.it>
2022-03-05 12:01:20 +0100

    add cv page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F75940',
+      },
+      screens: {
+        'print': {'raw': 'print'},
+      }
+    },
   },
   variants: {
     extend: {},
