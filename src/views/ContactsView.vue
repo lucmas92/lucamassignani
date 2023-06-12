c1c55ebc9460d388e6a893349423dfdb9096aa2c
Luca Massignani <Luca@proloco.local>
2023-06-12 22:27:16 +0200

    fix contact form 8

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -7,47 +7,51 @@
   <v-card rounded="0" elevation="0" class="pt-16 min-h-100">
     <Rotate text="Contacts"/>
     <Title style="z-index: -1" title="Contacts"/>
-    <v-form ref="contactForm" name="contactForm" data-netlify="true" @submit.prevent="handleSubmit">
-      <input type="hidden" name="form-name" value="contactForm"/>
+    <v-row class="px-12 mt-12 pb-16 mt-md-16 pt-lg-16">
+      <v-col class="mt-lg-12 mb-12 mb-md-0" cols="12" md="6">
+        <v-card-title class="mb-12">My Contacts</v-card-title>
+        <v-card-text>
+          <v-list>
+            <v-list-item link href="https://goo.gl/maps/i7i9WeK7oDskXXjR8?coh=178573&entry=tt">
+              <template v-slot:prepend>
+                <v-icon icon="mdi-map-marker"></v-icon>
+              </template>
+              Castelgomberto (VI), Italy
+            </v-list-item>
+            <v-list-item link href="tel:3495357197">
+              <template v-slot:prepend>
+                <v-icon icon="mdi-phone"></v-icon>
+              </template>
+              349 5357197
+            </v-list-item>
+            <v-list-item link href="mailto:massignani.luca@hotmail.it">
+              <template v-slot:prepend>
+                <v-icon icon="mdi-mail"></v-icon>
+              </template>
+              massignani.luca
+              <v-icon icon="mdi-at"></v-icon>
+              hotmail.it
+            </v-list-item>
+            <v-list-item link href="https://lucamassignani.it">
+              <template v-slot:prepend>
+                <v-icon icon="mdi-web"></v-icon>
+              </template>
+              lucamassignani.it
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+      </v-col>
 
-      <v-row class="px-12 mt-12 pb-16 mt-md-16 pt-lg-16">
-        <v-col class="mt-lg-12 mb-12 mb-md-0" cols="12" md="6">
-          <v-card-title class="mb-12">My Contacts</v-card-title>
-          <v-card-text>
-            <v-list>
-              <v-list-item link href="https://goo.gl/maps/i7i9WeK7oDskXXjR8?coh=178573&entry=tt">
-                <template v-slot:prepend>
-                  <v-icon icon="mdi-map-marker"></v-icon>
-                </template>
-                Castelgomberto (VI), Italy
-              </v-list-item>
-              <v-list-item link href="tel:3495357197">
-                <template v-slot:prepend>
-                  <v-icon icon="mdi-phone"></v-icon>
-                </template>
-                349 5357197
-              </v-list-item>
-              <v-list-item link href="mailto:massignani.luca@hotmail.it">
-                <template v-slot:prepend>
-                  <v-icon icon="mdi-mail"></v-icon>
-                </template>
-                massignani.luca
-                <v-icon icon="mdi-at"></v-icon>
-                hotmail.it
-              </v-list-item>
-              <v-list-item link href="https://lucamassignani.it">
-                <template v-slot:prepend>
-                  <v-icon icon="mdi-web"></v-icon>
-                </template>
-                lucamassignani.it
-              </v-list-item>
-            </v-list>
-          </v-card-text>
-        </v-col>
-        <v-col class="mt-lg-12 bg-transparent" cols="12" md="6">
-          <v-textarea name="Message" :rules="messageRules" class="bg-transparent my-2 my-md-4" rounded="0" label="Message"></v-textarea>
-          <v-text-field name="Name" :rules="nameRules" rounded="0" class="my-2 my-md-4" type="text" label="Name"></v-text-field>
-          <v-text-field name="Email" :rules="emailRules" rounded="0" class="my-2 my-md-4" type="email" label="Email"></v-text-field>
+      <v-col class="mt-lg-12 bg-transparent" cols="12" md="6">
+
+        <v-form ref="contactForm" name="contactForm" data-netlify="true" @submit.prevent="handleSubmit">
+          <input type="hidden" name="form-name" value="contactForm"/>
+          <v-textarea name="Message" :rules="messageRules" class="bg-transparent my-2 my-md-4" rounded="0"
+                      label="Message" v-model="formData.message"></v-textarea>
+          <v-text-field name="Name" :rules="nameRules" rounded="0" class="my-2 my-md-4" type="text"
+                        label="Name" v-model="formData.name"></v-text-field>
+          <v-text-field name="Email" :rules="emailRules" rounded="0" class="my-2 my-md-4" type="email"
+                        label="Email" v-model="formData.email"></v-text-field>
           <v-hover>
             <template v-slot:default="{ isHovering, props }">
               <v-btn type="submit" v-bind="props" class="px-8 text-uppercase" rounded="0" variant="outlined"
@@ -56,9 +60,9 @@
               </v-btn>
             </template>
           </v-hover>
-        </v-col>
-      </v-row>
-    </v-form>
+        </v-form>
+      </v-col>
+    </v-row>
 
   </v-card>
 </template>
@@ -73,6 +77,11 @@ export default defineComponent({
   name: 'ContactsView',
   data: () => ({
     valid: true,
+    formData: {
+      name: '',
+      email: '',
+      message: '',
+    },
     works: [],
     nameRules: [
       v => !!v || 'Name is required',
@@ -122,13 +131,11 @@ export default defineComponent({
       if (!validate['valid'])
         return
 
-      const myForm = e.target;
-      const formData = new FormData(myForm);
 
       fetch("/", {
         method: "POST",
         headers: {"Content-Type": "application/x-www-form-urlencoded"},
-        body: new URLSearchParams(formData).toString(),
+        body: new URLSearchParams(this.formData).toString(),
       })
         .then(() => this.$router.push('/thanks'))
         .catch((error) => alert(error));
